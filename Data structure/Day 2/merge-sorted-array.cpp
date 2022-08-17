20158b2906ac98c460960980806ffb5e429c1358
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-17 12:06:25 +0530

    Create merge-sorted-array.cpp

---
+++ Data structure/Day 2/merge-sorted-array.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+
+class Solution {
+public:
+	void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+		int c = 0;
+
+		for (int i = 0; i < n; i++) {
+			
+			nums1[m++] = nums2[i];
+
+		}
+		sort(nums1.begin(), nums1.end());
+	}
+		
+};
+
+
+int main() {
+
+    Solution sol;
+	
+	vector<int> nums1 = {1,2,3,0,0,0};
+	vector<int> nums2 = {2,5,6};
+	
+    int n = 3;
+	int m = 3;
+
+    sol.merge(nums1, m, nums2, n);
+
+    return 0;
+}
+
+
+
